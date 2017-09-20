5e92ac1636054ad51a2955d2d5047ad7cbaee29c
Pelle Almquist <per.almquist@gmail.com>
2017-09-20 14:03:53 +0200

    Consume mysql from Python asyncio

--- stack_python_asyncio/src/app.py
+++ stack_python_asyncio/src/app.py
@@ -1,6 +1,27 @@
 from sanic import Sanic, response
+from aiomysql.sa import create_engine
 
 app = Sanic(__name__)
+
+
+@app.route('/db')
+async def hello_db(request):
+    try:
+        async with create_engine(
+            user='ts_user',
+            password='ts_pw',
+            host='db_mysql',
+            db='ts_db'
+        ) as engine:
+            async with engine.acquire() as connection:
+                resultProxy = await connection.execute('SELECT 1')
+                result = await resultProxy.first()
+                if result and result[0] == 1:
+                    return response.text('Connected to database')
+    except:
+        pass
+
+    return response.text('Not connected to database')
 
 
 @app.route('/')
@@ -9,4 +30,4 @@ async def hello(request):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
