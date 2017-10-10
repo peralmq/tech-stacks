d00a4825b1ede3eda0732095c1b5668660ea3f45
Pelle Almquist <per.almquist@gmail.com>
2017-10-10 15:42:46 +0200

    Add API level tests

--- stack_kotlin_spark/src/test/kotlin/com/epidemicsound/TestServer.kt
+++ stack_kotlin_spark/src/test/kotlin/com/epidemicsound/TestServer.kt
@@ -1,5 +1,6 @@
 package com.epidemicsound
 
+import com.beust.klaxon.*
 import kotlin.test.*
 import org.junit.*
 import com.github.kittinunf.fuel.Fuel
@@ -20,7 +21,43 @@ class TestServer {
 
     @Test
     fun testRoot() {
-        val actual = Fuel.get("http://localhost:4567/").responseString().third.get()
-        assertEquals("Hello from Spark Kotlin!", actual)
+        val actual = Fuel.get("http://localhost:4567/").responseString()
+        assertEquals(200, actual.second.statusCode)
+        assertEquals("Hello from Spark Kotlin!", actual.third.get())
+    }
+
+    val createTrackBody = json {
+        mapOf("url" to "https://example.com")
+    }.toString()
+
+    @Test
+    fun testCreateTrack() {
+        val expected = json {
+            mapOf(
+                    "url" to "https://example.com",
+                    "id" to "1"
+
+            )
+        }
+        val actual = Fuel.post("http://localhost:4567/tracks/").body(createTrackBody).responseString()
+        assertEquals(201, actual.second.statusCode)
+        assertEquals(expected.toString(), actual.third.get())
+    }
+
+    @Test
+    fun testGetTrack() {
+        val (_, _, responseBody) = Fuel.post("http://localhost:4567/tracks/").body(createTrackBody).responseString()
+        val id = (Parser().parse(responseBody.get().byteInputStream()) as JsonObject).string("id")
+        val expected = json {
+            mapOf(
+                    "url" to "https://example.com",
+                    "id" to id
+
+            )
+        }
+        val actual = Fuel.get("http://localhost:4567/tracks/" + id).body(createTrackBody).responseString()
+        assertEquals(200, actual.second.statusCode)
+        assertEquals(expected.toString(), actual.third.get())
     }
 }
+
