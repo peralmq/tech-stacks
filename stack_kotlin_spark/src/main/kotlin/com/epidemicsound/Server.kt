f532b174e1b6255c74a22e312d3edad5ff370112
Pelle Almquist <per.almquist@gmail.com>
2017-10-10 17:23:50 +0200

    Refactor into controllers and models

--- stack_kotlin_spark/src/main/kotlin/com/epidemicsound/Server.kt
+++ stack_kotlin_spark/src/main/kotlin/com/epidemicsound/Server.kt
@@ -3,34 +3,56 @@ package com.epidemicsound
 import com.beust.klaxon.*
 import spark.*
 
-var storage = mutableMapOf<String, Map<String, *>>()
+var storage = mutableMapOf<String, Models.Track>()
+
 var counter = 0
+
+class Models {
+    class Track(val id: String, val url: String) {
+        companion object {
+            fun fromJsonString(id: String, s: String): Track {
+                val url = (Parser().parse(s.byteInputStream()) as JsonObject).string("url")
+                return Track(id, url ?: "")
+            }
+        }
+
+        fun toJsonString(): String {
+            return JsonObject(mapOf(
+                "id" to this.id,
+                "url" to this.url
+            )).toJsonString()
+        }
+    }
+}
+
+class Controllers {
+    fun root(req: Request, res: Response): String {
+        return "Hello from Spark Kotlin!"
+    }
+
+    fun createTrack(req: Request, res: Response): String {
+        val id = "${counter++}"
+        val track = Models.Track.fromJsonString(id, req.body())
+        storage[id] = track
+        res.status(201)
+        return track.toJsonString()
+    }
+
+    fun showTrack(req: Request, res: Response): Any {
+        val id = req.params("id")
+        return storage[id]?.let { track ->
+            res.status(200)
+            track.toJsonString()
+        } ?: res.status(404)
+    }
+}
 
 class Router {
     init {
-        Spark.get("/") { _, _ ->
-            "Hello from Spark Kotlin!"
-        }
-
-        Spark.post("/tracks/") { req, res ->
-            val id = "${counter++}"
-            val url = (Parser().parse(req.body().byteInputStream()) as JsonObject).string("url")
-            val data = mapOf(
-                    "id" to id,
-                    "url" to url
-            )
-            storage[id] = data
-            res.status(201)
-            JsonObject(data).toJsonString()
-        }
-
-       Spark.get("/tracks/:id") { req, res ->
-           val id = req.params("id")
-           storage[id]?.let { data ->
-               res.status(200)
-               JsonObject(data).toJsonString()
-           } ?: res.status(404)
-        }
+        val controllers = Controllers()
+        Spark.get("/", controllers::root)
+        Spark.post("/tracks/", controllers::createTrack)
+        Spark.get("/tracks/:id", controllers::showTrack)
     }
 }
 
